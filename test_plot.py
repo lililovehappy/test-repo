5ee2a4a90b91362a2a15656a0b22dd7daf81846c
lililovehappy <165114382+lililovehappy@users.noreply.github.com>
2024-05-11 15:17:04 +0800

    Add files via upload

---
+++ test_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+# 创建一些示例数据
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+# 绘制折线图
+plt.plot(x, y)
+plt.xlabel('X轴标签')
+plt.ylabel('Y轴标签')
+plt.title('示例折线图')
+plt.show()
